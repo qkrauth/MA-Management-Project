939c15aa29cb2d1dfaf7353f6540415f32022433
Quinten Krauth <quintenkrauth19@gmail.com>
2025-10-16 23:07:30 -0400

    database py logic

--- app/database.py
+++ app/database.py
@@ -1,4 +1,14 @@
 # database setup
 
-from flask_sqlalchemy import SQLAlchemy
-db = SQLAlchemy()
+from . import db
+
+# defines inventory table item model
+class InventoryItem(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    quantity = db.Column(db.Integer, nullable=False, default=0)
+    price = db.Column(db.Float, nullable=False)
+    description = db.Column(db.String(200))
+
+    def __repr__(self):
+        return f'<InventoryItem {self.name}>'
