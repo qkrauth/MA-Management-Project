4cf1e96fef1ee02cb1e841cda120df2e910bdeb7
Quinten Krauth <quintenkrauth19@gmail.com>
2025-10-16 22:53:12 -0400

    init logic for init.py root

--- app/__init__.py
+++ app/__init__.py
@@ -1,3 +1,5 @@
+# starts flask and connects everything
+
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
@@ -8,25 +10,18 @@ import os
 db = SQLAlchemy()
 ma = Marshmallow()
 
-# starts flask and connects everything
 def create_app():
+    load_dotenv()
     app = Flask(__name__, instance_relative_config=True)
-    app.config.from_object('instance.config.Config')
 
-    # Initialize extensions
+    #database
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///mashop.db'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
     db.init_app(app)
     ma.init_app(app)
 
-    # Register blueprints (routes)
     from .routes.inventory_routes import inventory_bp
-    from .routes.order_routes import order_bp
-    from .routes.invoice_routes import invoice_bp
-
-    app.register_blueprint(inventory_bp, url_prefix="/api/inventory")
-    app.register_blueprint(order_bp, url_prefix="/api/orders")
-    app.register_blueprint(invoice_bp, url_prefix="/api/invoices")
-
-    with app.app_context():
-        db.create_all()
+    app.register_blueprint(inventory_bp, url_prefix='api/inventory')
 
     return app
